b97a13bc1782157821c69f4dd508d337feae51ce
Vinayak Hakhu <39513628+thegammingttl@users.noreply.github.com>
2021-01-07 19:27:28 +1100

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,15 @@
+class Log extends BaseClass {
+    constructor(x, y, height, angle) {
+     
+      
+     super(x, y, 20, height);
+      
+    //  Angles
+      Matter.Body.setAngle(this.body, angle);
+// Loading Image
+this.image = loadImage("Sprites/wood2.png")
+    
+  
+    }
+  };
+  
---
+++ Pig.js
@@ -0,0 +1,11 @@
+class Pig extends BaseClass {
+    constructor(x, y) {
+      
+       super(x, y, 50, 50);
+      //  Loading Image
+       this.image = loadImage("Sprites/enemy.png")
+    
+    }
+  
+  };
+  
